09c0bc46526af2b6db0da48ca4df19a969a66151
Michael Rigoni <michael.rigoni@origin-nexus.com>
2020-06-09 12:19:18 +0200

    check directly in /proc/mounts if glusterfs mounted and force unmount if stale

--- glusterfs-volume/volume.go
+++ glusterfs-volume/volume.go
@@ -15,7 +15,6 @@ type Volume struct {
 	Options    map[string]string
 
 	Mountpoint string
-	mounted    bool
 }
 
 var ExecuteCommand = func(cmd string, args ...string) ([]byte, error) {
@@ -37,7 +36,6 @@ func (gv *Volume) Mount() error {
 	if output, err := ExecuteCommand("mount", args...); err != nil {
 		return fmt.Errorf("mount command execute failed: %v (%s)", err, output)
 	}
-	gv.mounted = true
 	return nil
 }
 
@@ -59,7 +57,7 @@ func (gv *Volume) getMountArgs() []string {
 }
 
 func (gv *Volume) Unmount() error {
-	if !gv.isMounted() {
+	if !InMtab(gv.Mountpoint) {
 		logrus.Debugf("'%v' not mounted, so not unmounting", gv.VolumeName)
 		return nil
 	}
@@ -68,31 +66,40 @@ func (gv *Volume) Unmount() error {
 	if err != nil {
 		return fmt.Errorf("umount command execute failed: %v (%s)", err, output)
 	}
-	gv.mounted = false
 	return nil
 }
 
 func (gv *Volume) isMounted() bool {
-	if !gv.mounted {
-		// check if already mounted
-		f, err := os.Open("/proc/mounts")
-		if err != nil {
-			logrus.Errorf("Failed to open '/proc/mounts': %v", err)
-			return gv.mounted
-		}
-		defer f.Close()
-		scanner := bufio.NewScanner(f)
-		for scanner.Scan() {
-			mount := strings.Split(scanner.Text(), " ")
-			if mount[1] == gv.Mountpoint {
-				gv.mounted = true
-				break
-			}
-		}
+	if !InMtab(gv.Mountpoint) {
+		return false
 	}
 
-	return gv.mounted
+	_, err := os.Stat(gv.Mountpoint)
+	if err == nil {
+		return true
+	}
+	// force unmount as it seems stale.
+	gv.Unmount()
+	return false
 }
+
+func InMtab(mountpoint string) bool {
+	f, err := os.Open("/proc/mounts")
+	if err != nil {
+		logrus.Errorf("Failed to open '/proc/mounts': %v", err)
+		return false
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		mount := strings.Split(scanner.Text(), " ")
+		if mount[1] == mountpoint {
+			return true
+		}
+	}
+	return false
+}
+
 func (gv *Volume) createMountpoint() error {
 	fi, err := os.Lstat(gv.Mountpoint)
 
